e01760f28c64e21e0b91eba59495ee65aa8046ee
Your Name <you@example.com>
2022-03-17 21:57:25 -0300

    new try

--- list.c
+++ list.c
@@ -69,15 +69,16 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
-    list->current = list->head;
-    pushCurrent(list,data);
-    /*Node * n = createNode(data);
+    Node * n = createNode(data);
     n->next = list->head; //Conectar el nodo a la lista.
     if (list->head != NULL) {
         list->head->prev = n; //Si hay elementos en la lista, conectar el primero al nodo nuevo.
     }
+    else {
+        list->tail = n; //Si no hay head, no hay elementos y el nuevo nodo será el último.
+    }
     list->head = n; //Declarar n como el nuevo head.
-    n->prev = NULL; //Declarar su puntero hacia el antecesor como NULL;*/
+    n->prev = NULL; //Declarar su puntero hacia el antecesor como NULL;
 }
 
 void pushBack(List * list, void * data) {
